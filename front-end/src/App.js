64c5ed821c7fa7d4693c5a868e8bd13784d046fc
Paul Raucci <paulraucci@Pauls-MacBook-Pro-4.local>
2019-11-21 16:46:47 -0500

    simple splash page

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import MainContent from "./component/MainContent";
+
+function App() {
+  return (
+    <div className="App">
+      <MainContent />
+    </div>
+  );
+}
+
+export default App;
