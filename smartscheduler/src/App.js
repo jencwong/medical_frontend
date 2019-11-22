0f9f55d1ce6a1e28e91eccada7ff39df6127d1a7
Kathryn Zaharek <kathrynzaharek@Kathryns-MBP.lan1>
2019-11-22 07:06:47 -0500

    Added comments field to update and new forms. Patient dashboard, NewAppt, UpdateAppt and ShowAppt - built out more and refined code. Added notes / thoughts to ShowAppt for discussion with team.

--- smartscheduler/src/App.js
+++ smartscheduler/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import './App.css';
 // import Header from './components/Header';
-// import MainContent from './components/MainContent';
+import MainContent from './components/MainContent';
 // import Footer from './components/Footer';
 
 function App() {
